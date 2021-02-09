2f95b7a31616914d0d7af1db9c3246b6b7db8c5b
Chris Pritchard <chris@grislygrotto.nz>
2021-02-09 19:56:04 +1300

    work on transitioning fog

--- Assets/Scripts/HexCellShaderData.cs
+++ Assets/Scripts/HexCellShaderData.cs
@@ -2,11 +2,17 @@
 namespace DarkDomains
 {
     using UnityEngine;
+    using System.Collections.Generic;
     
     public class HexCellShaderData : MonoBehaviour 
     {
         Texture2D cellTexture;
         Color32[] cellTextureData;
+
+        public bool ImmediateMode { get; set; } = true;
+
+        List<HexCell> transitioningCells = new List<HexCell>();
+        const float transitionSpeed = 255f;
 
         public void Initialise(int x, int z)
         {
@@ -28,6 +34,7 @@ namespace DarkDomains
                 for(var i = 0; i < cellTextureData.Length; i++)
                     cellTextureData[i] = new Color32(0, 0, 0, 0);
 
+            transitioningCells.Clear();
             enabled = true;
         }
 
@@ -39,16 +46,70 @@ namespace DarkDomains
 
         public void RefreshVisibility(HexCell cell)
         {
-            cellTextureData[cell.Index].r = (byte)(cell.IsVisible ? 255 : 0);
-            cellTextureData[cell.Index].g = (byte)(cell.IsExplored ? 255 : 0);
+            if(ImmediateMode)
+            {
+                cellTextureData[cell.Index].r = (byte)(cell.IsVisible ? 255 : 0);
+                cellTextureData[cell.Index].g = (byte)(cell.IsExplored ? 255 : 0);
+            }
+            else if (cellTextureData[cell.Index].b != 255)
+            {   
+                cellTextureData[cell.Index].b = 255; // just a flag to prevent re-adding
+                transitioningCells.Add(cell);
+            }
             enabled = true;
         }
 
         private void LateUpdate() 
         {
+            var delta = (int)(Time.deltaTime * transitionSpeed);
+            if(delta == 0)
+                delta = 1;
+
+            var next = new List<HexCell>(transitioningCells.Capacity);
+            foreach(var cell in transitioningCells)
+            {
+                if(UpdateCellData(cell, delta))
+                    next.Add(cell);
+            }
+            transitioningCells = next;
+
             cellTexture.SetPixels32(cellTextureData);
             cellTexture.Apply();
-            enabled = false;    
+            enabled = transitioningCells.Count > 0;
+        }
+
+        private bool UpdateCellData(HexCell cell, int delta)
+        {
+            var data = cellTextureData[cell.Index];
+            var stillUpdating = false;
+
+            if(cell.IsExplored && data.g < 255)
+            {
+                stillUpdating = true;
+                var t = data.g + delta;
+                data.g = (byte)(t >= 255 ? 255 : t);
+            }
+
+            if(cell.IsVisible)
+            {
+                if(data.r < 255)
+                {
+                    stillUpdating = true;
+                    var t = data.r + delta;
+                    data.r = (byte)(t >= 255 ? 255 : t);
+                }
+            }
+            else if (data.r > 0)
+            {
+                stillUpdating = true;
+                var t = data.r - delta;
+                data.r = (byte)(t < 0 ? 0 : t);
+            }
+
+            if(!stillUpdating)
+                data.b = 0; // clear updating flag
+            cellTextureData[cell.Index] = data;
+            return stillUpdating;
         }
     }
 }
