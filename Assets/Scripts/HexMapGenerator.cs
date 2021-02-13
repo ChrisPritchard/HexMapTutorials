39cf69116d5f388da95c36f8e2b70ced922f7723
Chris Pritchard <chris@grislygrotto.nz>
2021-02-13 13:56:19 +1300

    more work on generator and darkened water textures

--- Assets/Scripts/HexMapGenerator.cs
+++ Assets/Scripts/HexMapGenerator.cs
@@ -7,11 +7,108 @@ namespace DarkDomains
     {
         public HexGrid Grid;
 
+        [Range(0f, 0.5f)]
+        public float JitterProbability = 0.25f;
+
+        [Range(0f, 1f)]
+        public float HighRiseProbability = 0.25f;
+
+        [Range(20, 200)]
+        public int ChunkSizeMin = 30;
+
+        [Range(20, 200)]
+        public int ChunkSizeMax = 100;
+
+        [Range(5, 95)]
+        public int LandPercentage = 50;
+
+        [Range(1, 5)]
+        public int WaterLevel = 3;
+
+
+        private int cellCount;
+        private HexCellPriorityQueue searchFrontier;
+        private int searchFrontierPhase;
+
         public void GenerateMap (int x, int z)
         {
+            cellCount = x * z;
             Grid.CreateMap(x, z);
-            for(var i = 0; i < z; i++)
-                Grid.GetCell(x / 2, i).TerrainTypeIndex = 1;
+
+            if(searchFrontier == null)
+                searchFrontier = new HexCellPriorityQueue();
+
+            for(var i = 0; i < cellCount; i++)
+                Grid.GetCell(i).WaterLevel = (byte)WaterLevel;
+
+            CreateLand();
+            SetTerrainType();
+
+            for(var i = 0; i < cellCount; i++)
+                Grid.GetCell(i).SearchPhase = 0;
+        }
+
+        private void CreateLand()
+        {
+            var landBudget = Mathf.RoundToInt(cellCount * LandPercentage * 0.01f);
+
+            while(landBudget > 0)
+                landBudget = RaiseTerrain(Random.Range(ChunkSizeMin, ChunkSizeMax + 1), landBudget);
+        }
+
+        private void SetTerrainType()
+        {
+            for(var i = 0; i < cellCount; i++)
+            {
+                var cell = Grid.GetCell(i);
+                if(!cell.IsUnderwater)
+                    cell.TerrainTypeIndex = (byte)(cell.Elevation - cell.WaterLevel);
+            }
+        }
+
+        private int RaiseTerrain(int chunkSize, int budget)
+        {
+            searchFrontierPhase ++;
+
+            var firstCell = GetRandomCell();
+            firstCell.SearchPhase = searchFrontierPhase;
+            firstCell.Distance = 0;
+            firstCell.SearchHeuristic = 0;
+            searchFrontier.Enqueue(firstCell);
+
+            var centre = firstCell.Coordinates;
+
+            var rise = Random.value < HighRiseProbability ? 2 : 1;
+            var size = 0;
+            while (size < chunkSize && searchFrontier.Count > 0)
+            {
+                var current = searchFrontier.Dequeue();
+                var originalElevation = current.Elevation;
+
+                current.Elevation = (byte)(originalElevation + rise);
+                if(originalElevation < WaterLevel && current.Elevation >= WaterLevel && --budget == 0)
+                    break;
+                size ++;
+
+                for(var d = HexDirection.NE; d <= HexDirection.NW; d++)
+                {
+                    var neighbour = current.GetNeighbour(d);
+                    if(neighbour && neighbour.SearchPhase < searchFrontierPhase)
+                    {
+                        neighbour.SearchPhase = searchFrontierPhase;
+                        neighbour.Distance = neighbour.Coordinates.DistanceTo(centre);
+                        neighbour.SearchHeuristic = Random.value < JitterProbability ? 1 : 0;
+                        searchFrontier.Enqueue(neighbour);
+                    }
+                }
+            }
+            searchFrontier.Clear();
+            return budget;
+        }
+
+        private HexCell GetRandomCell()
+        {
+            return Grid.GetCell(Random.Range(0, cellCount));
         }
     }
 }
